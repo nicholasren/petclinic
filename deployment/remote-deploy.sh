263d72c45d272f42c85901f09f95f1da00f3d0e9
Nicholas Ren <nicholas85211@gmail.com>
2011-09-13 18:51:08 +0800

    xx

---
+++ deployment/remote-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#the previous line indedicates which intepretor should be used to intepret following scrip
+# this script is running on a go agent
+# jobs:
+#   1.copy artifcats to target node
+#   3.restart server
+export target_username=root
+export target_ip=10.29.2.21
+export target_deploy_path=/home/ubuntu/projects/petclinic/tomcat/server
+
+#copy release to target node
+scp  ../target/petclinic.war $target_username@$target_ip:$target_deploy_path/webapps/
+
+#deploy release
+ssh $target_username@$target_ip "bash $target_deploy_path/bin/shutdown.sh && $target_deploy_path/bin/startup.sh && tail -f $target_deploy_path/logs/catalina.out"
