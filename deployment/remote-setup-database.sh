4c2d1780623675797beb3a96dd0d7102246fd5d2
Nicholas Ren <nicholas85211@gmail.com>
2011-09-15 20:08:10 +0800

    remove exported veriable

--- deployment/remote-setup-database.sh
+++ deployment/remote-setup-database.sh
@@ -1,7 +1,7 @@
 
-export target_jdbc_url="jdbc:mysql://localhost:3306/mysql"
-export target_jdbc_username="root"
-export target_jdbc_password=""
+#export target_jdbc_url="jdbc:mysql://localhost:3306/mysql"
+#export target_jdbc_username="root"
+#export target_jdbc_password=""
 #set up database
 #mvn sql:execute
 
