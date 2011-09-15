bc2bfa5a353f3df5423f8033387ff7ee75ccba77
Nicholas Ren <nicholas85211@gmail.com>
2011-09-15 20:03:03 +0800

    db auto setup

--- deployment/remote-setup-database.sh
+++ deployment/remote-setup-database.sh
@@ -1,8 +1,8 @@
 
-#export jdbc_url="jdbc:mysql://localhost:3306/mysql"
-#export jdbc_username="root"
-#export jdbc_password=""
+export target_jdbc_url="jdbc:mysql://localhost:3306/mysql"
+export target_jdbc_username="root"
+export target_jdbc_password=""
 #set up database
 #mvn sql:execute
 
-mvn sql:execute -Durl=$jdbc_url -Dusername=$jdbc_username -Dpassword=$jdbc_password
+mvn sql:execute -Durl=$target_jdbc_url -Dusername=$target_jdbc_username -Dpassword=$target_jdbc_password -X
