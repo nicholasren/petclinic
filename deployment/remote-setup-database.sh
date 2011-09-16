ce662246a379b6aa4600caaa62e86f42f0cfe25f
Nicholas Ren <nicholas85211@gmail.com>
2011-09-16 12:44:07 +0800

    change database set url to default db url

--- deployment/remote-setup-database.sh
+++ deployment/remote-setup-database.sh
@@ -1,8 +1,8 @@
 
-#export target_jdbc_url="jdbc:mysql://localhost:3306/mysql"
+#export target_root_jdbc_url="jdbc:mysql://localhost:3306/mysql"
 #export target_jdbc_username="root"
 #export target_jdbc_password=""
 #set up database
 #mvn sql:execute
 
-mvn sql:execute -Durl=$target_jdbc_url -Dusername=$target_jdbc_username -Dpassword=$target_jdbc_password -X
+mvn sql:execute -Durl=$target_root_jdbc_url -Dusername=$target_jdbc_username -Dpassword=$target_jdbc_password -X
