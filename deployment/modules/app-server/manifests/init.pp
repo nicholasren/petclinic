9b316acbb81b959298bc8cb72f3b5b7d1c12adff
Nicholas Ren <nicholas85211@gmail.com>
2011-09-16 14:23:22 +0800

    remove unwanted puppet scripts

--- deployment/modules/app-server/manifests/init.pp
+++ deployment/modules/app-server/manifests/init.pp
@@ -3,7 +3,7 @@ class app-server {
     $project_path = "~/IdeaProjects/petclinic/"
     
     # install mysql, openjdk, tomcat
-    $wantedpackages = ["mysql-server", "openjdk-6-jdk", "tomcat6"]
+    $wantedpackages = ["mysql-server", "openjdk-6-jdk"]
 
     package { $wantedpackages: ensure => installed }
 
@@ -14,30 +14,4 @@ class app-server {
         require => Package["mysql-server"],
         restart => true
     }
-    
-    #create db script
-    file { "/tmp/initDB.txt":
-            source=> "${project_path}/src/main/resources/db/mysql/initDB.txt",
-    }
-    
-    #populate db script
-    file { "/tmp/populateDB.txt":
-            source=> "${project_path}/src/main/resources/db/mysql/populateDB.txt",
-    }
-
-    #create db schema
-    exec { "init-db":
-        command => "mysql < /tmp/initDB.txt",
-        path =>"/bin:/usr/bin",
-    }
-    
-    #populate db
-    exec { "populate-db":
-        command => "mysql petclinic < /tmp/populateDB.txt",
-        path =>"/bin:/usr/bin",
-    }
-    
-    Exec['init-db'] <~ File['/tmp/initDB.txt'] <- Service['mysql']
-    Exec['populate-db'] <~ File['/tmp/populateDB.txt'] <- Service['mysql']
-    
 }
