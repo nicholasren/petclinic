80e2d62a7c283fc5fee8f9bb9b9a88743c44176c
Nicholas Ren <nicholas85211@gmail.com>
2011-09-17 19:33:33 +0800

    x

--- deployment/modules/app-server/manifests/init.pp
+++ deployment/modules/app-server/manifests/init.pp
@@ -1,17 +1,29 @@
 class app-server {
 
-    $project_path = "~/IdeaProjects/petclinic/"
-    
     # install mysql, openjdk, tomcat
     $wantedpackages = ["mysql-server", "openjdk-6-jdk", "tomcat6"]
 
-    package { $wantedpackages: ensure => installed }
+    #package { $wantedpackages: ensure => installed }
 
-    service { "mysql":
-        ensure => running,
-        hasstatus => true,
-        hasrestart => true,
-        require => Package["mysql-server"],
-        restart => true
+
+    #change bind-address to load ip
+
+    #start up service
+    #service { "mysql":
+    #    ensure => running,
+    #    hasstatus => true,
+    #    hasrestart => true,
+    #    require => Package["mysql-server"],
+    #    restart => true
+    #}
+
+    #create database user
+    file {"/tmp/create_db.sql":
+        source => "puppet:///modules/app-server/create_db.sql",
+        #source => "/Users/reaapi/IdeaProjects/petclinic/deployment/modules/app-server/files/create_db.sql",
+        ensure => present,
     }
+
 }
+
+class{'app-server':}
--- deployment/remote-setup-database.sh
+++ deployment/remote-setup-database.sh
@@ -1,8 +1,8 @@
 
-#export target_root_jdbc_url="jdbc:mysql://localhost:3306/mysql"
-#export target_jdbc_username="root"
-#export target_jdbc_password=""
+export target_root_jdbc_url="jdbc:mysql://192.168.137.122:3306/mysql"
+export target_jdbc_username="pc"
+export target_jdbc_password="pc"
 #set up database
-#mvn sql:execute
+
 
 mvn sql:execute -Durl=$target_root_jdbc_url -Dusername=$target_jdbc_username -Dpassword=$target_jdbc_password -X
