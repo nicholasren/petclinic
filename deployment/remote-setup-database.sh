fa77fd53e7c67fb30098020bd822d747bf380acb
Nicholas Ren <nicholas85211@gmail.com>
2011-09-17 21:43:09 +0800

    change

--- deployment/remote-setup-database.sh
+++ deployment/remote-setup-database.sh
@@ -1,8 +1,8 @@
 
-#export target_root_jdbc_url="jdbc:mysql://192.168.137.60:3306/petclinic"
+#export target_jdbc_url="jdbc:mysql://192.168.137.60:3306/petclinic"
 #export target_jdbc_username="pc"
 #export target_jdbc_password="pc"
 #set up database
 
 
-mvn sql:execute -Durl=$target_root_jdbc_url -Dusername=$target_jdbc_username -Dpassword=$target_jdbc_password 
+mvn sql:execute -Durl=$target_jdbc_url -Dusername=$target_jdbc_username -Dpassword=$target_jdbc_password 
